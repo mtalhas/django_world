77e5d5ac4a8f259c1a4d939389427eb256f3baed
4mtalhas <talha.siddiqui@iaim.ca>
2022-07-26 04:50:32 -0400

    django world

--- django_world/settings.py
+++ django_world/settings.py
@@ -118,9 +118,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / "static"]
-STATIC_ROOT = STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_ROOT = "staticfiles"
 STATICFILES_STORAGE = "django.contrib.staticfiles.storage.StaticFilesStorage"
-DISTILL_DIR = '/public'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
